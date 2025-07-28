060723cf79cbcc9f6417766d00d616ce0f6c4a1d
Ansari kaish <204324277+kaish02723@users.noreply.github.com>
2025-07-28 09:24:29 +0530

    SharePrefrence

---
+++ share_prefrence_flutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:share_prefrence_flutter/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
